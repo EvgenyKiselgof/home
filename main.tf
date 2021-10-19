2d11a5e3a9baa1141a2c4f2baa95a37477f8acd8
Evgeny <evgeny.kiselgof@cognyte.com>
2021-10-19 18:30:26 +0300

    another test with tf

--- main.tf
+++ main.tf
@@ -14,20 +14,22 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.11"
 }
 
 data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "evgeny-ha-chekmx"
+  cluster_name = "eks-chekmx-evg"
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
 
-  name                 = "chekmx-vpc"
+  name                 = "k8s-vpc"
   cidr                 = "172.16.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
@@ -49,8 +51,7 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "12.2.0"
-
+  version = "17.1.0"
   cluster_name    = "${local.cluster_name}"
   cluster_version = "1.20"
   subnets         = module.vpc.private_subnets
@@ -63,10 +64,9 @@ module "eks" {
       max_capacity     = 10
       min_capacity     = 1
 
-      instance_type = "m5.large"
+      instance_type = "t2.micro"
     }
   }
 
   write_kubeconfig   = true
-  config_output_path = "./"
 }
